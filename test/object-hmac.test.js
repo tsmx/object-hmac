67f535c0b4a9b5a18bbb276b08d10291f642570f
tsmx <strs@freenet.de>
2021-09-17 19:59:22 +0200

    Jest updated

--- test/object-hmac.test.js
+++ test/object-hmac.test.js
@@ -13,7 +13,7 @@ describe('object-hmac test suite', () => {
         testObjects = require('./testobjects');
     });
 
-    it('tests a successful HMAC creation and verification - end-to-end', async (done) => {
+    it('tests a successful HMAC creation and verification - end-to-end', async () => {
         let person = {
             name: 'Max',
             age: 32
@@ -21,108 +21,89 @@ describe('object-hmac test suite', () => {
         objectHmac.createHmac(person, testKey);
         expect(person[testHmacAttribute]).toBeDefined();
         expect(objectHmac.verifyHmac(person, testKey)).toBeTruthy();
-        done();
     });
 
-    it('tests a successful HMAC calculation', async (done) => {
+    it('tests a successful HMAC calculation', async () => {
         const hmac = objectHmac.calculateHmac(testObjects.testObject, testKey);
         expect(hmac).toStrictEqual(testHmac);
-        done();
     });
 
-    it('tests a successful HMAC creation for an object', async (done) => {
+    it('tests a successful HMAC creation for an object', async () => {
         let obj = testObjects.testObject;
         expect(obj[testHmacAttribute]).toBeUndefined();
         objectHmac.createHmac(obj, testKey);
         expect(obj[testHmacAttribute]).toStrictEqual(testHmac);
-        done();
     });
 
-    it('tests a successful HMAC creation for an object with a different attribute name', async (done) => {
+    it('tests a successful HMAC creation for an object with a different attribute name', async () => {
         let obj = testObjects.testObject;
         expect(obj[testHmacAttributeDifferent]).toBeUndefined();
         objectHmac.createHmac(obj, testKey, testHmacAttributeDifferent);
         expect(obj[testHmacAttributeDifferent]).toStrictEqual(testHmac);
-        done();
     });
 
-    it('test a successful HMAC verification', async (done) => {
+    it('test a successful HMAC verification', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmac, testKey)).toBeTruthy();
-        done();
     });
 
-    it('test a successful HMAC verification - JSON with changed attribute order', async (done) => {
+    it('test a successful HMAC verification - JSON with changed attribute order', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmacChangedOrder, testKey)).toBeTruthy();
-        done();
     });
 
-    it('test a successful HMAC verification - JSON with changed attribute order in a subobject', async (done) => {
+    it('test a successful HMAC verification - JSON with changed attribute order in a subobject', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmacChangedSubobjectOrder, testKey)).toBeTruthy();
-        done();
     });
 
-    it('test a successful HMAC verification - JSON loaded from a file via require', async (done) => {
+    it('test a successful HMAC verification - JSON loaded from a file via require', async () => {
         let testObj = require('./testobject');
         expect(objectHmac.verifyHmac(testObj, testKey)).toBeTruthy();
-        done();
     });
 
-    it('test a successful HMAC verification - JSON loaded from a file via fs', async (done) => {
+    it('test a successful HMAC verification - JSON loaded from a file via fs', async () => {
         let fs = require('fs');
         let path = require('path');
         let testObj = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'testobject.json'), 'utf8'));
         expect(objectHmac.verifyHmac(testObj, testKey)).toBeTruthy();
-        done();
     });
 
-    it('test a successful HMAC verification with a different attribute name', async (done) => {
+    it('test a successful HMAC verification with a different attribute name', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmacDifferentAttribute, testKey, testHmacAttributeDifferent)).toBeTruthy();
-        done();
     });
 
-    it('test a failed HMAC verification - different attribute name', async (done) => {
+    it('test a failed HMAC verification - different attribute name', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmac, testKey, testHmacAttributeDifferent)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - no HMAC provided for object', async (done) => {
+    it('test a failed HMAC verification - no HMAC provided for object', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObject, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - changed object attribute', async (done) => {
+    it('test a failed HMAC verification - changed object attribute', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectChangedAttribute, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - changed array order', async (done) => {
+    it('test a failed HMAC verification - changed array order', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmacChangedArrayOrder, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - added object attribute', async (done) => {
+    it('test a failed HMAC verification - added object attribute', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectAddedAttribute, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - deleted object attribute', async (done) => {
+    it('test a failed HMAC verification - deleted object attribute', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectDeletedAttribute, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - manipulated HMAC', async (done) => {
+    it('test a failed HMAC verification - manipulated HMAC', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectManipulatedHmac, testKey)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - wrong key', async (done) => {
+    it('test a failed HMAC verification - wrong key', async () => {
         expect(objectHmac.verifyHmac(testObjects.testObjectWithHmac, testKeyBroken)).toBeFalsy();
-        done();
     });
 
-    it('test a failed HMAC verification - obj is null', async (done) => {
+    it('test a failed HMAC verification - obj is null', async () => {
         expect(objectHmac.verifyHmac(null, testKey)).toBeFalsy();
-        done();
     });
 
 });
